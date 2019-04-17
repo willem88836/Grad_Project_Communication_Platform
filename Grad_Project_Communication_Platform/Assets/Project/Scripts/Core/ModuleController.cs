2f0fc13fbe50bba270e18ff2d41558a01a800a55
Willem Meijer <willem1998m@hotmail.com>
2019-04-17 10:49:29 +0200

    Implemented Module selection client side.

--- Grad_Project_Communication_Platform/Assets/Project/Scripts/Core/ModuleController.cs
+++ Grad_Project_Communication_Platform/Assets/Project/Scripts/Core/ModuleController.cs
@@ -1,7 +1,12 @@
-﻿using UnityEngine;
+﻿using System;
+using UnityEngine;
+using UnityEngine.UI;
 
 public class ModuleController : MonoBehaviour
 {
+	public GameObject ModuleButtonPrefab;
+	public Transform ButtonContainer;
+
 	private NetworkClient networkClient;
 	private RoleplayModule selectedModule = RoleplayModule.None;
 	private bool inQueue = false;
@@ -9,7 +14,24 @@ public class ModuleController : MonoBehaviour
 	public void Initialize(NetworkClient networkClient)
 	{
 		this.networkClient = networkClient;
+		CreateButtons();
 	}
+
+	private void CreateButtons()
+	{
+		Array modules = Enum.GetValues(typeof(RoleplayModule));
+		foreach(RoleplayModule module in modules)
+		{
+			GameObject newButton = Instantiate(ModuleButtonPrefab, ButtonContainer);
+
+			Text textField = newButton.transform.GetChild(0).GetComponent<Text>();
+			textField.text = module.ToString();
+
+			Button button = newButton.GetComponent<Button>();
+			button.onClick.AddListener(delegate { SelectModule(module); });
+		}
+	}
+
 
 	public void SelectModule(RoleplayModule module)
 	{
@@ -30,10 +52,25 @@ public class ModuleController : MonoBehaviour
 		NetworkMessage dequeueMessage = new NetworkMessage(NetworkMessageType.Dequeue, networkClient.ClientId, "", selectedModule.ToString());
 		networkClient.SendMessage(dequeueMessage);
 	}
+
+	public void ToggleLockIn()
+	{
+		if (inQueue)
+		{
+			LockOutModule();
+		}
+		else
+		{
+			LockInModule();
+		}
+	}
 }
 
 // TODO: add all the modules. 
 public enum RoleplayModule
 {
 	None,
+	Paraphrasing, 
+	Follow_Up_Questions,
+	Open_Questions
 };

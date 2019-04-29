c9671c291da87166058e3e05307315663ec7baa8
Willem Meijer <willem1998m@hotmail.com>
2019-04-29 18:36:13 +0200

    Updated Module buttons.

--- Grad_Project_Communication_Platform/Assets/Project/Scripts/Modules/ModuleController.cs
+++ Grad_Project_Communication_Platform/Assets/Project/Scripts/Modules/ModuleController.cs
@@ -1,37 +1,19 @@
-﻿using System;
-using UnityEngine;
-using UnityEngine.UI;
+﻿using UnityEngine;
 
 public class ModuleController : MonoBehaviour
 {
 	public ScreenController ScreenController;
 	public GameObject ModuleButtonPrefab;
 	public Transform ButtonContainer;
-	public int textIndex;
 
+	private RoleplayModule selectedModule = RoleplayModule.FreePlay;
 	private NetworkClient networkClient;
-	public RoleplayModule SelectedModule { get; set; } = RoleplayModule.FreePlay;
 	private bool inQueue = false;
+
 
 	public void Initialize(NetworkClient networkClient)
 	{
 		this.networkClient = networkClient;
-		CreateButtons();
-	}
-
-	private void CreateButtons()
-	{
-		Array modules = Enum.GetValues(typeof(RoleplayModule));
-		foreach(RoleplayModule module in modules)
-		{
-			GameObject newButton = Instantiate(ModuleButtonPrefab, ButtonContainer);
-
-			Text textField = newButton.transform.GetChild(textIndex).GetComponent<Text>();
-			textField.text = module.ToString();
-
-			Button button = newButton.GetComponent<Button>();
-			button.onClick.AddListener(delegate { SelectModule(module); });
-		}
 	}
 
 
@@ -40,21 +22,21 @@ public class ModuleController : MonoBehaviour
 		if (inQueue)
 			return;
 
-		SelectedModule = module;
+		selectedModule = module;
 		ScreenController.SwitchScreenToModuleBriefing();
 	}
 
 	public void LockInModule()
 	{
 		inQueue = true;
-		NetworkMessage queueMessage = new NetworkMessage(NetworkMessageType.Enqueue, networkClient.ClientId, "", SelectedModule.ToString());
+		NetworkMessage queueMessage = new NetworkMessage(NetworkMessageType.Enqueue, networkClient.ClientId, "", selectedModule.ToString());
 		networkClient.SendMessage(queueMessage);
 	}
 
 	public void LockOutModule()
 	{
 		inQueue = false;
-		NetworkMessage dequeueMessage = new NetworkMessage(NetworkMessageType.Dequeue, networkClient.ClientId, "", SelectedModule.ToString());
+		NetworkMessage dequeueMessage = new NetworkMessage(NetworkMessageType.Dequeue, networkClient.ClientId, "", selectedModule.ToString());
 		networkClient.SendMessage(dequeueMessage);
 	}
 

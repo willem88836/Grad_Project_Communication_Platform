60f12f306a0957cb242f9cc1354726558f97b560
Willem Meijer <willem1998m@hotmail.com>
2019-05-12 16:32:37 +0200

    Users now have logs referring to the conversations they have had.
    - updated SaveLoad utility.
    - HistoryManager now loads evaluations using user log.
    - Added EnsureFileDirectories (to ensure all sharedstring paths exist.
    - updated complete evaluation generator to save the user logs.

--- Grad_Project_Communication_Platform/Assets/Project/Scripts/History/HistoryManager.cs
+++ Grad_Project_Communication_Platform/Assets/Project/Scripts/History/HistoryManager.cs
@@ -1,6 +1,7 @@
-﻿using Framework.Features.Json;
-using Framework.ScriptableObjects.Variables;
+﻿using Framework.ScriptableObjects.Variables;
 using Framework.Storage;
+using UnityEngine;
+using JsonUtility = Framework.Features.Json.JsonUtility;
 
 namespace Project.History
 {
@@ -8,35 +9,49 @@ namespace Project.History
 	{
 		public int ChunkSize = 5;
 		public SharedString CompleteEvaluationName;
+		public SharedString UserLogsName;
 
 
 		public void OnHistoryLogsRequested(Participant participant, string serializedIndex)
 		{
 			int index = int.Parse(serializedIndex);
 
+			string[] ids = LoadUserLogs(participant.Id);
+
+
+			int count = Mathf.Clamp(Mathf.Min(ChunkSize, ids.Length - index), 0, int.MaxValue);
+
 			SerializedHistory serializedHistory = new SerializedHistory()
 			{
 				Index = index,
-				SerializedHistoryLogs = new string[ChunkSize]
+				SerializedHistoryLogs = new string[count]
 			};
 
-			// loads the available logs.
-			for (int i = 0; i < ChunkSize; i++)
+			for (int i = index; i < count; i++)
 			{
-				int j = i + index;
-
-				string name = string.Format(CompleteEvaluationName.Value, j);
+				string id = ids[i];
+				string name = string.Format(CompleteEvaluationName.Value, id);
 				SaveLoad.Load(name, out string log);
 
 				if (log == null)
+				{
+					Debug.LogWarning("Missing Evaluation Entry: " + id);
 					break;
-
-				serializedHistory.SerializedHistoryLogs[i] = log;
+				}
+				
+				serializedHistory.SerializedHistoryLogs[i - index] = log;
 			}
 
 			string json = JsonUtility.ToJson(serializedHistory);
 			NetworkMessage message = new NetworkMessage(NetworkMessageType.RequestHistoryLogs, "", participant.Id, json);
 			Manager.SendMessage(message, participant.IP);
+		}
+
+
+		private string[] LoadUserLogs(string userId)
+		{
+			SaveLoad.Load(string.Format(UserLogsName.Value, userId), out string data);
+			return data.Split(new char[] { ',' }, System.StringSplitOptions.RemoveEmptyEntries);
 		}
 	}
 }
--- Grad_Project_Communication_Platform/Assets/Project/Scripts/Roleplay/Evaluation/CompleteEvaluationGenerator.cs
+++ Grad_Project_Communication_Platform/Assets/Project/Scripts/Roleplay/Evaluation/CompleteEvaluationGenerator.cs
@@ -12,6 +12,7 @@ public class CompleteEvaluationGenerator : ApplicationController<NetworkServer>
 {
 	public SharedString RoleplaySaveName;
 	public SharedString CompleteEvaluationName;
+	public SharedString UserLogsName;
 
 	private Dictionary<string, CaseEvaluation> acquiredEvaluations = new Dictionary<string, CaseEvaluation>();
 
@@ -34,39 +35,52 @@ public class CompleteEvaluationGenerator : ApplicationController<NetworkServer>
 		string id = caseEvaluation.Id;
 		if(acquiredEvaluations.ContainsKey(id))
 		{
-			// Removes the case from the dictionary.
-			CaseEvaluation other = acquiredEvaluations[id];
-			acquiredEvaluations.Remove(id);
-
-			// converts the stored roleplay to an object.
-			string roleplayJson;
-			string fileName = string.Format(RoleplaySaveName.Value, id);
-			SaveLoad.Load(fileName, out roleplayJson);
-			RoleplayDescription roleplayDescription = JsonUtility.FromJson<RoleplayDescription>(roleplayJson);
-
-			// serialized the complete evaluation.
-			bool isUserA = id == roleplayDescription.UserA.Id;
-			CompleteCaseEvaluation completeCaseEvaluation = new CompleteCaseEvaluation()
-			{
-				RoleplayDescription = roleplayDescription,
-				EvaluationUserA = isUserA ? caseEvaluation : other,
-				EvaluationUserB = isUserA ? other : caseEvaluation,
-				TimeStamp = DateTime.Now.ToString()
-			};
-
-			string completeEvalJson = JsonUtility.ToJson(completeCaseEvaluation);
-
-			SaveLoad.Save(completeEvalJson, string.Format(CompleteEvaluationName.Value, id));
-
-			SendMessageTo(roleplayDescription.UserA, completeEvalJson);
-			SendMessageTo(roleplayDescription.UserB, completeEvalJson);
-
-			SaveLoad.Remove(fileName);
+			GenerateCompleteEvaluation(id, caseEvaluation);
 		}
 		else
 		{
 			acquiredEvaluations.Add(id, caseEvaluation);
 		}
+	}
+
+	private void GenerateCompleteEvaluation(string id, CaseEvaluation caseEvaluation)
+	{
+		// Removes the case from the dictionary.
+		CaseEvaluation other = acquiredEvaluations[id];
+		acquiredEvaluations.Remove(id);
+
+		// Converts the stored roleplay to an object.
+		string roleplayJson;
+		string fileName = string.Format(RoleplaySaveName.Value, id);
+		SaveLoad.Load(fileName, out roleplayJson);
+		RoleplayDescription roleplayDescription = JsonUtility.FromJson<RoleplayDescription>(roleplayJson);
+
+		// serialized the complete evaluation.
+		bool isUserA = id == roleplayDescription.UserA.Id;
+		CompleteCaseEvaluation completeCaseEvaluation = new CompleteCaseEvaluation()
+		{
+			RoleplayDescription = roleplayDescription,
+			EvaluationUserA = isUserA ? caseEvaluation : other,
+			EvaluationUserB = isUserA ? other : caseEvaluation,
+			TimeStamp = DateTime.Now.ToString()
+		};
+
+		string completeEvalJson = JsonUtility.ToJson(completeCaseEvaluation);
+
+		SaveLoad.Save(completeEvalJson, string.Format(CompleteEvaluationName.Value, id));
+
+		SendMessageTo(roleplayDescription.UserA, completeEvalJson);
+		SendMessageTo(roleplayDescription.UserB, completeEvalJson);
+
+		AddIdToUserLog(roleplayDescription.UserA.Id, id);
+		AddIdToUserLog(roleplayDescription.UserB.Id, id);
+
+		SaveLoad.Remove(fileName);
+	}
+
+	private void AddIdToUserLog(string userId, string evaluationId)
+	{
+		SaveLoad.Append(string.Format(UserLogsName.Value, userId), string.Format("{0},", evaluationId));
 	}
 
 	private void SendMessageTo(Participant user, string message)
